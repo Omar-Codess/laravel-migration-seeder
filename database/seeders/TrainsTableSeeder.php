779496a243d3f158d62d7e184aa75e5b825dae41
Omar-Codess <omartaki8@gmail.com>
2022-12-21 09:35:10 +0100

    aggiunto faker

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,11 @@
+<?php
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TrainsTableSeeder extends Seeder {
+    public function run(Faker $faker) {
+        $
+    }
+}
