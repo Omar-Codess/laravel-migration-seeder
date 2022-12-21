fc3237ada2eb4a1ecdbae642a8faf1b2d7ffd19c
Omar-Codess <omartaki8@gmail.com>
2022-12-21 17:29:09 +0100

    aggiunti dati fake al db

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -1,11 +1,28 @@
 <?php
+
 namespace Database\Seeders;
 
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 
-class TrainsTableSeeder extends Seeder {
-    public function run(Faker $faker) {
-        $
+class TrainsTableSeeder extends Seeder
+{
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $newTrain = new Train();
+            $newTrain->company = $faker->randomElement(['Trenitalia', 'Trenord']);
+            $newTrain->departure_station = $faker->city();
+            $newTrain->arrival_station = $faker->city();
+            $newTrain->departure_time = $faker->dateTimeBetween('-1 week', '+1 week');
+            $newTrain->arrival_time = $faker->dateTimeInInterval($newTrain->departure_time, "+20 hour");
+            $newTrain->train_code = $faker->bothify('####');
+            $newTrain->wagon_number = $faker->randomDigitNotNull(0);
+            $newTrain->in_time = $faker->numberBetween(0, 1);
+            $newTrain->cancelled = $faker->numberBetween(0, 1);
+            $newTrain->save();
+        }
     }
 }
